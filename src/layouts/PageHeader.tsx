5aa305177a7f56943a6a6d3dc049104f27b2eb8c
ACHii <achii@Achinthas-MacBook-Air.local>
2024-03-27 22:57:42 +0530

    PageHeader searchbar

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,30 +1,83 @@
-import { Bell, Menu, Upload, User } from 'lucide-react'
-import logo from '../assets/logo.svg'
-import Button from '../components/Button'
+import { ArrowLeft, Bell, Menu, Mic, Search, Upload, User } from "lucide-react"
+import logo from "../assets/logo.svg"
+import Button from "../components/Button"
+import { useState } from "react"
 
 const PageHeader = () => {
-  return <div className="flex justify-between gap-10 pt-2 mx-4 mb-6 lg:gap-20">
-    <div className="flex items-center flex-shrink-0 gap-4">
-      <Button variant="ghost" size="icon">
-        <Menu />
-      </Button>
-      <a href="/">
-        <img src={logo} alt="Logo" className='h-6' />
-      </a>
+  const [showFullWidthSearch, setShowFullWidthSearch] = useState(false)
+
+  return (
+    <div className="flex justify-between gap-10 pt-2 mx-4 mb-6 lg:gap-20">
+      <div
+        className={`flex items-center flex-shrink-0 gap-4 ${
+          showFullWidthSearch ? "hidden" : "flex"
+        }`}
+      >
+        <Button variant="ghost" size="icon">
+          <Menu />
+        </Button>
+        <a href="/">
+          <img src={logo} alt="Logo" className="h-6" />
+        </a>
+      </div>
+      <form
+        className={`items-center justify-center flex-grow gap-4 ${
+          showFullWidthSearch ? "flex" : "hidden md:flex"
+        }`}
+      >
+        {showFullWidthSearch && (
+          <Button
+            onClick={() => setShowFullWidthSearch(false)}
+            type="button"
+            size="icon"
+            variant="ghost"
+            className="flex-shrink-0"
+          >
+            <ArrowLeft />
+          </Button>
+        )}
+        <div className="flex flex-grow max-w-[600px]">
+          <input
+            type="search"
+            placeholder="Search"
+            className="w-full px-4 py-1 text-lg border rounded-l-full shadow-inner outline-none border-secondary-border shadow-secondary focus:border-blue-500"
+          />
+          <Button className="flex-shrink-0 px-4 py-2 border border-l-0 rounded-r-full border-secondary-border">
+            <Search />
+          </Button>
+        </div>
+        <Button type="button" size="icon" className="flex-shrink-0">
+          <Mic />
+        </Button>
+      </form>
+      <div
+        className={`flex-shrink-0 md:gap-2 ${
+          showFullWidthSearch ? "hidden" : "flex"
+        }`}
+      >
+        <Button
+          onClick={() => setShowFullWidthSearch(true)}
+          variant="ghost"
+          size="icon"
+          className="md:hidden"
+        >
+          <Search />
+        </Button>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Mic />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Upload />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Bell />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <User />
+        </Button>
+      </div>
     </div>
-    <div></div>
-    <div className='flex flex-shrink-0 md:gap-2'>
-      <Button variant="ghost" size="icon">
-        <Upload />
-      </Button>
-      <Button variant="ghost" size="icon">
-        <Bell />
-      </Button>
-      <Button variant="ghost" size="icon">
-        <User />
-      </Button>
-    </div>
-  </div>
+  )
 }
 
 export default PageHeader
