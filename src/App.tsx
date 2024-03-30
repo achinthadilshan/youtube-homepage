07b13e2c6606c206117f446fb051cf2ebac1ead3
ACHii <>
2024-03-30 23:15:20 +0530

    video grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import CategoryPills from "./components/CategoryPills"
-import { categories } from "./data/home"
+import { categories, videos } from "./data/home"
 import PageHeader from "./layouts/PageHeader"
+import VideoGridItem from "./components/VIdeoGridItem"
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0])
-  
+
   return (
     <div className="flex flex-col max-h-screen">
       <PageHeader />
@@ -13,7 +14,16 @@ function App() {
         <div>Sidebar</div>
         <div className="px-8 pb-4 overflow-x-hidden">
           <div className="sticky top-0 z-10 pb-4 bg-white">
-            <CategoryPills  selectedCategory={selectedCategory} onSelect={setSelectedCategory} categories={categories} />
+            <CategoryPills
+              selectedCategory={selectedCategory}
+              onSelect={setSelectedCategory}
+              categories={categories}
+            />
+          </div>
+          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+            {videos.map((video) => (
+              <VideoGridItem key={video.id} {...video} />
+            ))}
           </div>
         </div>
       </div>
